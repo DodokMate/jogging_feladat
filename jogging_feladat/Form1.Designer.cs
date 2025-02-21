1afcfbb75fcb3fb53cf40f1e4e4f77d6e7d5513d
DodokMate <dodok.mate@gmail.com>
2025-02-21 09:49:08 +0100

    Kész feladat!

--- jogging_feladat/Form1.Designer.cs
+++ jogging_feladat/Form1.Designer.cs
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(218, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 290);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Afrika",
+            "Ázsia",
+            "Európa"});
+            this.comboBox1.Location = new System.Drawing.Point(434, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(434, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adatok";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(218, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Országok";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(434, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kontinensek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(843, 539);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- jogging_feladat/Form1.cs
+++ jogging_feladat/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,74 @@ namespace jogging_feladat
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                comboBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public List<string[]>Lista = new List<string[]>();
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                comboBox1.Text = "Afrika";
+                listBox1.Items.Clear();
+                Lista.Clear();
+                StreamReader sr = new StreamReader("forras_afrika.txt");
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    Lista.Add(sor);
+                    listBox1.Items.Add(sor[0]);
+                }
+                sr.Close();
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                comboBox1.Text = "Ázsia";
+                listBox1.Items.Clear();
+                Lista.Clear();
+                StreamReader sr = new StreamReader("forras_azsia.txt");
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor1 = sr.ReadLine().Split(';');
+                    Lista.Add(sor1);
+                    listBox1.Items.Add(sor1[0]);
+                }
+                sr.Close();
+            }
+            else if (comboBox1.SelectedIndex == 2) 
+            {
+                comboBox1.Text = "Európa";
+                listBox1.Items.Clear();
+                Lista.Clear();
+                StreamReader sr = new StreamReader("forras_europa.txt");
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor2 = sr.ReadLine().Split(';');
+                    Lista.Add(sor2);
+                    listBox1.Items.Add(sor2[0]);
+                }
+                sr.Close();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = $"Ország: {Lista[listBox1.SelectedIndex][0]} \nFőváros: {Lista[listBox1.SelectedIndex][1]} \nTerület (km2): {Lista[listBox1.SelectedIndex][2]} \nNépesség: {Lista[listBox1.SelectedIndex][3]} \nNépsűrűség (fő/km): {Lista[listBox1.SelectedIndex][4]}";
         }
     }
 }
